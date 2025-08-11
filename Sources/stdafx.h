e4c2ee7bb205eb7ee65f0fa0451d81ddcf31a866
baba_masafumi <fimasu5249@gmail.com>
2025-08-11 15:46:24 +0900

    [Sources] stdafx.hを用意

---
+++ Sources/stdafx.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <cstddef>
+#include <utility>
+#include <memory>
+#include <atomic>
+#include <functional>
+#include <optional>
+#include <variant>
+#include <bit>
+#include <format>
+#include <algorithm>
+#include <iostream>
+#include <coroutine>
+#include <type_traits>
+#include <concepts>
+
+#include <string>
+#include <string_view>
+#include <array>
+#include <vector>
+#include <span>
+#include <tuple>
+#include <list>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <ranges>
+
+using namespace std::literals;
